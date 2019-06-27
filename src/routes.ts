3eaa9833bfd6dd8f4e031037f74e6e00b8e7e307
Ana Alice <anaalicefm18@gmail.com>
2019-06-27 18:37:14 -0300

    User crud routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,9 @@ import UserController from './controllers/UserController'
 const routes = Router()
 
 routes.get('/users', UserController.index)
+routes.get('/users/:id', UserController.show)
 routes.post('/users', UserController.create)
+routes.put('/users/:id', UserController.update)
+routes.delete('/users/:id', UserController.destroy)
 
 export default routes
