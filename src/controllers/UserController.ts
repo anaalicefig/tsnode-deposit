98b455394caeef136f63772c7089608709469b72
Ana Alice <anaalicefm18@gmail.com>
2019-06-27 18:35:35 -0300

    User crud

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 
-import User from '../schemas/User'
+import { User } from '../schemas/User'
 
 class UserController {
     public async index (req: Request, res: Response): Promise<Response> {
@@ -9,10 +9,28 @@ class UserController {
         return res.json(users)
     }
 
+    public async show(req: Request, res: Response): Promise<Response> {
+        const user = await User.findById(req.params.id)
+
+        return res.json(user)
+    }
+
     public async create(req: Request, res: Response) {
         const user = await User.create(req.body)
         
         return res.json(user)
+    }
+
+    public async update(req: Request, res: Response) {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+        return res.json(user)
+    }
+
+    public async destroy(req: Request, res: Response) {
+        await User.findByIdAndRemove(req.params.id, req.body)
+
+        return res.status(200)
     }
 }
 
